4b3abf65792fee50fb2be34cd4d35c428c8d6198
github yadac <doruji1982@gmail.com>
2017-09-10 22:30:43 +0900

    add wpf application

---
+++ MCP70483wpf/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+
+namespace MCP70483wpf
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            string content = await httpClient
+                .GetStringAsync("http://www.microsoft.com")
+                .ConfigureAwait(false);
+
+            // when configureAwait is "false", this code cause exception.
+            this.label1.Content = content;
+        }
+
+        // you should also avoid returning void from an async method except when its an event handler.
+        // 
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            string content = await httpClient
+                .GetStringAsync("http://www.microsoft.com")
+                .ConfigureAwait(false);
+
+            using (FileStream sourceStream
+                = new FileStream("temp.html", FileMode.Create, FileAccess.Write, FileShare.None, 4096, useAsync: true))
+            {
+                byte[] encodedText = Encoding.Unicode.GetBytes(content);
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length)
+                    .ConfigureAwait(false);
+            }
+        }
+    }
+}

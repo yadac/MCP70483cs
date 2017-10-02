fa8cc1ae846276a158a176331cd7941cdbcbc30f
yadac <doruji1982@gmail.com>
2017-10-03 08:33:27 +0900

    blocking collection

---
+++ MCP70483cs/Example1-28.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs
+{
+    class Example1_28
+    {
+        public static void DoProc()
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+            Task read = Task.Run(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine(col.Take());
+                }
+            });
+
+            Task write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s)) break;
+                    col.Add(s);
+                }
+            });
+            write.Wait();
+        }
+    }
+}

ea3c74a84c67c713d5469d3f82b535e8a255001a
yadac <doruji1982@gmail.com>
2017-10-05 08:55:40 +0900

    1-33 concurrentQueue

---
+++ MCP70483cs/Example1-33.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs
+{
+    class Example1_33
+    {
+        public static void DoProc()
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            queue.Enqueue(42);
+
+            int result;
+
+            // queue is first-in, first-out.
+            if (queue.TryDequeue(out result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}

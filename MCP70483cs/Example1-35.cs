5119b2a366346c5fc7abc33faa61414131ffb0f0
yadac <doruji1982@gmail.com>
2017-10-05 23:04:46 +0900

    1-35 non-atomic variables

---
+++ MCP70483cs/Example1-35.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs
+{
+    class Example1_35
+    {
+        public static void DoProc()
+        {
+            int n = 0;
+
+            // its wrong example.
+            // multithread access variable at same time.
+
+            var up = Task.Run(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    // msil = read, store, write
+                    // there is a possible to overwrite other thread updates.
+                    n++;
+                }
+            });
+            for (int i = 0; i < 1000000; i++)
+            {
+                n--;
+            }
+            up.Wait();
+            Console.WriteLine(n); // not zero
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -17,7 +17,7 @@ namespace MCP70483cs
 
         public static void Main(string[] args)
         {
-            Example1_34.DoProc();
+            Example1_35.DoProc();
             Console.ReadLine();
         }
     }

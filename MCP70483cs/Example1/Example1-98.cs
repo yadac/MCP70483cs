1eec8180757bc1bf2f3661e2f8aeb3b5e27b53bd
yadac <doruji1982@gmail.com>
2017-10-11 22:46:07 +0900

    custom exception

---
+++ MCP70483cs/Example1/Example1-98.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example1
+{
+    class Example1_98
+    {
+
+    }
+
+    [Serializable]
+    public class OrderProcessException98 : Exception, ISerializable
+    {
+        public int OrderId { get; private set; }
+
+        public OrderProcessException98(int orderId)
+        {
+            OrderId = orderId;
+            this.HelpLink = "https://msdn.microsoft.com/ja-jp/library/system.exception(v=vs.110).aspx";
+        }
+
+        public OrderProcessException98(int orderId, string message): base(message)
+        {
+            OrderId = orderId;
+            this.HelpLink = "https://msdn.microsoft.com/ja-jp/library/system.exception(v=vs.110).aspx";
+        }
+
+        public OrderProcessException98(int orderId, string message, Exception innerException) : 
+            base(message, innerException)
+        {
+            OrderId = orderId;
+            this.HelpLink = "https://msdn.microsoft.com/ja-jp/library/system.exception(v=vs.110).aspx";
+        }
+
+        protected OrderProcessException98(SerializationInfo info, StreamingContext context)
+        {
+            OrderId = (int) info.GetValue("OrderId", typeof(int));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("OrderId", OrderId, typeof(int));
+        }
+
+
+    }
+}

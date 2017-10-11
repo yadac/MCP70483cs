9c0dcdc851d945e7ac7de4d14b2762b4a0459cd4
yadac <doruji1982@gmail.com>
2017-10-11 21:04:14 +0900

    throw exception

---
+++ MCP70483cs/Example1/Example1-95.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example1
+{
+    class Example1_95
+    {
+        public static void DoProc()
+        {
+            try
+            {
+                string s = "hello";
+                int i = int.Parse(s);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw; // rethrow the original exception, keep stacktrace
+            }
+        }
+    }
+}
---
+++ MCP70483cs/Example1/Example1-96.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example1
+{
+    class Example1_96
+    {
+        public static void DoProc()
+        {
+            try
+            {
+                new Example1_96().ProcessOrder();
+            }
+            catch (OrderProcessException e)
+            {
+                Console.WriteLine($"outer exception");
+                Console.WriteLine(e);
+                Console.WriteLine();
+                Console.WriteLine($"inner exception");
+                Console.WriteLine(e.InnerException);
+                // throw;
+            }
+        }
+
+        public void ProcessOrder()
+        {
+            try
+            {
+                string s = "hello";
+                int i = int.Parse(s);
+
+            }
+            catch (FormatException e)
+            {
+                throw new OrderProcessException("error while processing order", e); ;
+            }
+        }
+    }
+
+    public class OrderProcessException : Exception
+    {
+        public OrderProcessException(string message, Exception e) : base(message, e)
+        {
+        }
+    }
+}

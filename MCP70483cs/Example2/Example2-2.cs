920a831214ccd7fe34461ffd9a245800a998e83e
yadac <doruji1982@gmail.com>
2017-10-12 09:01:36 +0900

    struct keyword

---
+++ MCP70483cs/Example2/Example2-2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example2
+{
+    class Example2_2
+    {
+        public static void DoProc()
+        {
+
+        }
+    }
+
+    public struct Point2
+    {
+        public int x, y;
+
+        public Point2(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+    }
+
+}

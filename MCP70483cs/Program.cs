0a701eee8135730bad86472be6b4c2845fcdfdf3
github yadac <doruji1982@gmail.com>
2017-10-15 16:06:04 +0900

    extention method

--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -18,7 +18,7 @@ namespace MCP70483cs
 
         public static void Main(string[] args)
         {
-            Example2_14.DoProc();
+            Example2_17.DoProc();
             Console.ReadLine();
         }
     }

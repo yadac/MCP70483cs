9f3fb7d1cd421951e32d6095839f5e1507588c8f
yadac <doruji1982@gmail.com>
2017-10-18 22:22:52 +0900

    base classes

--- MCP70483cs/Example2/Example2-43.cs
+++ MCP70483cs/Example2/Example2-43.cs
@@ -10,55 +10,74 @@ namespace MCP70483cs.Example2
     {
         public static void DoProc()
         {
-            var human = new HumanRepository().FindById(1);
-            Console.WriteLine(human?.Name);
-            var humans = new HumanRepository().GetItems();
-            foreach (var h in humans)
-            {
-                Console.WriteLine(h.Name);
-            }
-        }
-    }
-
-    public interface IRepository243<T> where T : class
-    {
-        T FindById(int id);
-        IEnumerable<T> GetItems();
-    }
-
-    public class HumanRepository : IRepository243<Human243>
-    {
-        private Human243 _instance; // to be singleton
-
-        public HumanRepository()
-        {
-            _instance = new Human243();
-            _instance.Humans = new List<Human243>()
+            // test data
+            IEnumerable<Human243> humans = new List<Human243>()
             {
                 new Human243() {Id = 1, Name = "taro", Age = 20},
                 new Human243() {Id = 2, Name = "hanako", Age = 18}
             };
-        }
 
-        public Human243 FindById(int id)
-        {
-            return _instance.Humans.FirstOrDefault(i => i.Id == id);
-        }
+            IRepository243<Human243> repos = new HumanRepository(humans);
+            Console.WriteLine(repos.FindById(1).Name);
+            foreach (var human in repos.GetItems())
+            {
+                Console.WriteLine("----");
+                Console.WriteLine(human.Id);
+                Console.WriteLine(human.Name);
+                Console.WriteLine(human.Age);
+            }
 
-        public IEnumerable<Human243> GetItems()
-        {
-            return _instance.Humans;
         }
     }
 
+    public interface IRepository243<T> where T : IEntity243
+    {
+        IEnumerable<T> Members { get; set; }
+        T FindById(int id);
+        IEnumerable<T> GetItems();
+    }
 
-    public class Human243
+    public class Repository243<T> : IRepository243<T> where T : IEntity243
+    {
+        public IEnumerable<T> Members { get; set; }
+
+        public Repository243(IEnumerable<T> members)
+        {
+            Members = members;
+        }
+
+
+        public T FindById(int id)
+        {
+            return Members.First();
+        }
+
+        public IEnumerable<T> GetItems()
+        {
+            return Members;
+        }
+    }
+
+    public class HumanRepository : Repository243<Human243>
+    {
+
+        public HumanRepository(IEnumerable<Human243> members) : base(members)
+        {            
+        }
+
+    }
+
+    public interface IEntity243
+    {
+        int Id { get; set; }
+    }
+
+
+    public class Human243 : IEntity243
     {
         public int Id { get; set; }
         public int Age { get; set; }
         public string Name { get; set; }
-
-        public List<Human243> Humans { get; set; }
     }
 
 }
---
+++ MCP70483cs/Example2/Example2-46.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example2
+{
+    class Example2_46
+    {
+        public static void DoProc()
+        {
+
+        }
+    }
+
+    interface IEntity
+    {
+        int Id { get; set; }
+    }
+
+    class Repository246<T> where T : IEntity
+    {
+        protected IEnumerable<T> _elements;
+
+        public Repository246(IEnumerable<T> elements)
+        {
+            _elements = elements;
+        }
+
+        public T FindById(int id)
+        {
+            return _elements.FirstOrDefault(e => e.Id == id);
+        }
+
+        public IEnumerable<T> All()
+        {
+            return _elements;
+        }
+    }
+
+    class Order246 : IEntity
+    {
+        public int Id { get; set; }
+
+    }
+
+    class OrderRepository246 : Repository246<Order246>
+    {
+        public OrderRepository246(IEnumerable<Order246> orders) : base(orders)
+        {
+        }
+
+        public IEnumerable<Order246> FilterOrdersOnAmount(decimal amount)
+        {
+            List<Order246> result = null;
+            return result;
+        }
+
+    }
+}

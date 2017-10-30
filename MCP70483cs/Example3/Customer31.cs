446de5bccb6950f0c14a94ef4cc640136b8c4017
yadac <doruji1982@gmail.com>
2017-10-30 09:02:43 +0900

    validation

---
+++ MCP70483cs/Example3/Customer31.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MCP70483cs.Example3
+{
+    class Customer31
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        public Address31 ShippingAddress { get; set; }
+
+        [Required]
+        public Address31 BillingAddress { get; set; }
+
+
+    }
+
+    public class Address31
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string AddressLine1 { get; set; }
+        [Required, MaxLength(20)]
+        public string AddressLine2 { get; set; }
+        [Required, MaxLength(20)]
+        public string City { get; set; }
+
+        [RegularExpression(@"^[1-9][0-9]3\s?[a-zA-Z]{2}$")]
+        public string ZipCode { get; set; }
+    }
+}

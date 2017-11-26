e854616d1ba5332ac839ae54003d8ed118bd431b
yadac <doruji1982@gmail.com>
2017-11-26 16:19:34 +0900

    event log

---
+++ MCP70483cs/Example3/Example3-49.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example3
+{
+    class Example3_49
+    {
+        public static void DoProc()
+        {
+            if (!(EventLog.SourceExists("mySource")))
+            {
+                EventLog.CreateEventSource("mySource", "myNewLog");
+                Console.WriteLine("CreatedEventSource");
+                Console.WriteLine("Please restart application");
+                Console.ReadKey();
+                return;
+            }
+            // can't show "myNewLog" in event viewer. 
+            EventLog myLog = new EventLog();
+            myLog.Source = "mySource";
+            myLog.WriteEntry("Log Event!");
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -11,7 +11,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            Example3_47.DoProc();
+            Example3_49.DoProc();
             // commented, instead of Ctrl + F5
             // Console.ReadLine(); 
         }

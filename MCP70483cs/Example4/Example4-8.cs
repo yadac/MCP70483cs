c853913ded1b1196828c22c528f85be1d1ba2e89
yadac <doruji1982@gmail.com>
2017-12-03 15:00:59 +0900

    fileinfo

---
+++ MCP70483cs/Example4/Example4-8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_8
+    {
+        public static void DoProc()
+        {
+            string path = @"c:\temp\test.txt";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            FileInfo fileInfo = new FileInfo(path);
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+        }
+    }
+}

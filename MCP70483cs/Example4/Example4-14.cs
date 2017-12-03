ab77f963c24e6cfcaea4d2cf6f566f6ad7ed5977
yadac <doruji1982@gmail.com>
2017-12-03 18:13:13 +0900

    stream writer/reader

--- MCP70483cs/Example4/Example4-14.cs
+++ MCP70483cs/Example4/Example4-14.cs
@@ -11,12 +11,38 @@ namespace MCP70483cs.Example4
     {
         public static void DoProc()
         {
+
             string path = @"c:\temp\csharp\test.dat";
-            using (FileStream fs = File.Create(path))
+            File.Delete(path);
+
+            //using (FileStream fs = File.Create(path))
+            //{
+            //    string myValue = "myvalue";
+            //    byte[] data = Encoding.UTF8.GetBytes(myValue);
+            //    fs.Write(data, 0, data.Length);
+            //}
+
+            using (StreamWriter sw = File.CreateText(path))
             {
                 string myValue = "myvalue";
-                byte[] data = Encoding.UTF8.GetBytes(myValue);
-                fs.Write(data, 0, data.Length);
+                sw.Write(myValue);
+            }
+
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] data = new byte[fs.Length];
+                for (int i = 0; i < fs.Length; i++)
+                {
+                    data[i] = (byte)fs.ReadByte();
+                }
+                Console.Write("convert bytes to string:");
+                Console.WriteLine(Encoding.UTF8.GetString(data));
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                Console.Write("read lien:");
+                Console.WriteLine(sr.ReadLine());
             }
         }
     }

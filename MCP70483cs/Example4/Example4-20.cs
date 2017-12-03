542d890b7ba7018035ae879d7db0a5291a6c6096
yadac <doruji1982@gmail.com>
2017-12-03 20:10:47 +0900

    stream with networking

---
+++ MCP70483cs/Example4/Example4-20.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_20
+    {
+        public static string DoProc()
+        {
+            string path = @"c:\temp\csharp\test.dat";
+
+            if (File.Exists(path))
+            {
+                try
+                {
+                    return File.ReadAllText(path);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                }
+                catch (FileNotFoundException)
+                {
+                    
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ MCP70483cs/Example4/Example4-22.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_22
+    {
+        static Stopwatch sw = new Stopwatch();
+        public static void DoProc()
+        {
+            WebRequest request = WebRequest.Create("https://www.amazon.co.jp/");
+
+            sw.Start();
+            WebResponse response = request.GetResponse();
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            StreamReader responseStream = new StreamReader(response.GetResponseStream());
+            string responseText = responseStream.ReadToEnd();
+            Console.WriteLine(responseText);
+            response.Close();
+        }
+    }
+}

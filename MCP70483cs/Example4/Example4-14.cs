c36e38df7853570e9387610bfd4c3a9e6ac07695
yadac <doruji1982@gmail.com>
2017-12-03 17:51:56 +0900

    stream

---
+++ MCP70483cs/Example4/Example4-14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_14
+    {
+        public static void DoProc()
+        {
+            string path = @"c:\temp\csharp\test.dat";
+            using (FileStream fs = File.Create(path))
+            {
+                string myValue = "myvalue";
+                byte[] data = Encoding.UTF8.GetBytes(myValue);
+                fs.Write(data, 0, data.Length);
+            }
+        }
+    }
+}

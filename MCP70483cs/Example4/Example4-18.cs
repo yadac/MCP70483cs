8e1391f0a6872b6080ba915d1eaa9a2ef5033bca
yadac <doruji1982@gmail.com>
2017-12-03 18:26:47 +0900

    gzipstream

---
+++ MCP70483cs/Example4/Example4-18.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_18
+    {
+        public static void DoProc()
+        {
+            string folder = @"c:\temp";
+            string uncompressedFilePath = Path.Combine(folder, "uncompressed.dat");
+            string compressedFilePath = Path.Combine(folder, "compressed.gz");
+
+            // data
+            byte[] dataToCompress = 
+                Enumerable.Repeat((byte) 'a', 1024 * 1024).ToArray();
+
+            // create UnCompressedFile
+            using (FileStream unCompFs = File.Create(uncompressedFilePath))
+            {
+                unCompFs.Write(dataToCompress, 0, dataToCompress.Length);
+            }
+
+            // create CompressedFile by GZip
+            using (FileStream compFs = File.Create(compressedFilePath))
+            {
+                // gzipstream constructor receive other type stream.
+                using (GZipStream compStream = new GZipStream(compFs, CompressionMode.Compress))
+                {
+                    compStream.Write(dataToCompress, 0, dataToCompress.Length);
+                }
+            }
+            
+            // confirm
+            FileInfo uncompressFile = new FileInfo(uncompressedFilePath);
+            FileInfo compressFile = new FileInfo(compressedFilePath);
+            Console.WriteLine(uncompressFile.Length);
+            Console.WriteLine(compressFile.Length);
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -12,7 +12,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            Example4_14.DoProc();
+            Example4_18.DoProc();
             // commented, instead of Ctrl + F5
             // Console.ReadLine();
             //

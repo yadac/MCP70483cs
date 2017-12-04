be3ea5d17d26a04bc178a1b5e0919f1af1c7d75b
yadac <doruji1982@gmail.com>
2017-12-04 09:44:02 +0900

    task return value

--- MCP70483cs/Example4/Example4-26.cs
+++ MCP70483cs/Example4/Example4-26.cs
@@ -11,21 +11,13 @@ namespace MCP70483cs.Example4
     {
         public static void DoProc()
         {
-            Task task = Task.Run(() =>
-            {
-                var instance = new Example4_26();
-                instance.ExecuteMultipleRequestsInParallel();
-            });
+            var instance = new Example4_26();
+            Task<string> ret = instance.ExecuteMultipleRequestsInParallel();
             Console.WriteLine("*** after call");
-            task.Wait();
-            task.ContinueWith((s) =>
-            {
-                Console.WriteLine(s);
-            });
-
+            Console.WriteLine(ret.Result);
         }
 
-        public async Task<string[]> ExecuteMultipleRequestsInParallel()
+        public async Task<string> ExecuteMultipleRequestsInParallel()
         {
             Console.WriteLine("*** called method");
 
@@ -35,12 +27,8 @@ namespace MCP70483cs.Example4
             Task<string> msdn = client.GetStringAsync("http://msdn.microsoft.com");
             Task<string> blog = client.GetStringAsync("http://blogs.msdn.com");
 
-            string microsoft2 = await client.GetStringAsync("http://www.microsoft.com");
-            Console.WriteLine(microsoft2);
-
-            string[] s = await Task.WhenAll(microsoft, msdn, blog);
-            
-            return s;
+            await Task.WhenAll(microsoft, msdn, blog);
+            return microsoft.Result;
 
         }
     }

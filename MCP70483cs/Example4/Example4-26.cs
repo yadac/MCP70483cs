147368f2175c62b511992b28fefbbf0d34ef4d13
yadac <doruji1982@gmail.com>
2017-12-04 09:36:58 +0900

    task return

---
+++ MCP70483cs/Example4/Example4-26.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_26
+    {
+        public static void DoProc()
+        {
+            Task task = Task.Run(() =>
+            {
+                var instance = new Example4_26();
+                instance.ExecuteMultipleRequestsInParallel();
+            });
+            Console.WriteLine("*** after call");
+            task.Wait();
+            task.ContinueWith((s) =>
+            {
+                Console.WriteLine(s);
+            });
+
+        }
+
+        public async Task<string[]> ExecuteMultipleRequestsInParallel()
+        {
+            Console.WriteLine("*** called method");
+
+            HttpClient client = new HttpClient();
+
+            Task<string> microsoft = client.GetStringAsync("http://www.microsoft.com");
+            Task<string> msdn = client.GetStringAsync("http://msdn.microsoft.com");
+            Task<string> blog = client.GetStringAsync("http://blogs.msdn.com");
+
+            string microsoft2 = await client.GetStringAsync("http://www.microsoft.com");
+            Console.WriteLine(microsoft2);
+
+            string[] s = await Task.WhenAll(microsoft, msdn, blog);
+            
+            return s;
+
+        }
+    }
+}

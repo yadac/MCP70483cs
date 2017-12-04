56f4d26c801785723a68ab0999689890a19aae01
yadac <doruji1982@gmail.com>
2017-12-04 22:30:12 +0900

    work with sqlserver compact

---
+++ MCP70483cs/Example4/Example4-28.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlServerCe;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_28
+    {
+        public static void DoProc()
+        {
+            // common way to create connection string.
+            var builder = new SqlConnectionStringBuilder
+            {
+                // move to configuration file next.
+                DataSource = @"C:\Users\yosuke.adachi\source\WebSites\DemoWebPage\App_Data\StarterSite.sdf",
+                PersistSecurityInfo = false,
+            };
+            var cs = builder.ToString();
+            Console.WriteLine(cs);
+
+            // implements IDisposable.
+            using (SqlCeConnection connection = new SqlCeConnection(cs))
+            {
+                connection.Open();
+            }
+            
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -12,7 +12,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            Example4_26.DoProc();
+            Example4_28.DoProc();
             // commented, instead of Ctrl + F5
             // Console.ReadLine();
             //

d4b03870cec40e2625690ef7aaaf78470a3dde33
yadac <doruji1982@gmail.com>
2017-12-06 15:37:57 +0900

    change db path

---
+++ MCP70483cs/sqlcommands/DataInsert.sql
@@ -0,0 +1,6 @@
+﻿insert into [dbo].[Employees] values (1,'yosuke','adachi');
+insert into [dbo].[Employees] values (2,'tsuyoshi','kondo');
+insert into [dbo].[Employees] values (3,'shutaro','takahashi');
+insert into [dbo].[Employees] values (4,'toru','mizuno');
+insert into [dbo].[Employees] values (5,'natsuko','nakagawa');
+insert into [dbo].[Employees] values (6,'mokichi','saito');

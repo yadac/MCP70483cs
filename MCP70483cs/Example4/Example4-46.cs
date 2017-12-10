8e48711194c0dc93783ffeac96011499633a936a
yadac <doruji1982@gmail.com>
2017-12-10 17:14:17 +0900

    xpathnavigator

---
+++ MCP70483cs/Example4/Example4-46.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_46
+    {
+        public static void DoProc()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"Example4-42.xml");
+
+            XPathNavigator nav = doc.CreateNavigator();
+            string query = "//people/person[@firstName='Taro']";
+            XPathNodeIterator iterator = nav.Select(query);
+
+            Console.WriteLine($"iterator.Count : {iterator.Count}"); // display 1
+
+            while (iterator.MoveNext())
+            {
+                string firstName = iterator.Current.GetAttribute("firstName","");
+                string lastName = iterator.Current.GetAttribute("lastName", "");
+                Console.WriteLine($"Name : {firstName} {lastName}");
+            }
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -12,7 +12,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            Example4_45.DoProc();
+            Example4_46.DoProc();
         }
     }
 }

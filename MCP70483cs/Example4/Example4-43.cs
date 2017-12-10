5e5478a052021e6409bdfa8fb4f1c21c7c615d21
yadac <doruji1982@gmail.com>
2017-12-10 15:55:02 +0900

    xmlreader

---
+++ MCP70483cs/Example4/Example4-43.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_43
+    {
+        public static void DoProc()
+        {
+            using (XmlReader reader =
+                XmlReader.Create(@"Example4-42.xml", new XmlReaderSettings()
+                {
+                    IgnoreWhitespace = true
+                }))
+            {
+                reader.MoveToContent();
+
+                reader.ReadStartElement("people");
+                string firstName = reader.GetAttribute("firstName");
+                string lastName = reader.GetAttribute("lastName");
+                Console.WriteLine($"Person: {firstName} {lastName}");
+
+                // readstartelementは一足飛びに指定できない
+                // people -> person -> contactdetails というように階層を辿る
+                Console.WriteLine("ContactDetail");
+                reader.ReadStartElement("person");
+                reader.ReadStartElement("contactdetails");
+                string emailAddress = reader.ReadString();
+                Console.WriteLine($"Email address: {emailAddress}");
+            }
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -12,7 +12,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            Example4_40.DoProc();
+            Example4_43.DoProc();
         }
     }
 }

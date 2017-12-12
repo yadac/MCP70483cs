b578904326ca311b653bae8066f7f656ebef4f61
yadac <doruji1982@gmail.com>
2017-12-12 11:00:57 +0900

    log4net

--- MCP70483cs/Labo/Labo1.cs
+++ MCP70483cs/Labo/Labo1.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace MCP70483cs.Labo
 {
     public class Labo1
     {
+        private static readonly ILog _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         public static void DoProc(string[] args)
         {
             string s = string.Format($"args = {string.Join(",", args)}");
+            _logger.Debug(s);
             Console.WriteLine(s);
         }
     }

926dccd85e95a69972567547b3f586458bc57d63
yadac <doruji1982@gmail.com>
2017-12-13 19:40:41 +0900

    linq

---
+++ MCP70483cs/Example4/Example4-54.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_54
+    {
+        public static void DoProc()
+        {
+            int[] data = { 1, 2, 5, 8, 11 };
+
+            var result = from d in data where d % 2 == 0 select d;
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-------------");
+
+            foreach (var i in data.Where(d => d % 2 != 0))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

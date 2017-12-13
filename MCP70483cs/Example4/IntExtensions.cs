5a2009efad946dda2d9a1a07318e2284fcd5a847
yadac <doruji1982@gmail.com>
2017-12-13 19:29:41 +0900

    extension methods

---
+++ MCP70483cs/Example4/IntExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    public static class IntExtensions
+    {
+        // extension methods
+        // 既存クラスにコンパイルなしでメソッドを追加できる感じ
+        public static int Multiply(this int x, int y)
+        {
+            return x * y;
+        }
+    }
+}

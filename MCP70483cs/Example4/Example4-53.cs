ab1ae0b5197ad664185ad13f21fd5a395de7b458
yadac <doruji1982@gmail.com>
2017-12-13 19:34:43 +0900

    anonymous type

---
+++ MCP70483cs/Example4/Example4-53.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    class Example4_53
+    {
+        public static void DoProc()
+        {
+            // anonymous type, javascriptみたい..
+            var person = new 
+            {
+                FirstName = "john",
+                LastName = "Doe",
+            };
+
+            Console.WriteLine(person);
+        }
+    }
+}

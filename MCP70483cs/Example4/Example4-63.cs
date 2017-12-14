c99f38ce1f93605d859f01d7ce821fc8227906a7
yadac <doruji1982@gmail.com>
2017-12-14 22:24:05 +0900

    yield where implement

---
+++ MCP70483cs/Example4/Example4-63.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Example4
+{
+    public static class Example4_63
+    {
+        public static void DoProc()
+        {
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source
+            , Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -13,7 +13,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            Example4_62.DoProc();
+            Example4_63.DoProc();
         }
     }
 }

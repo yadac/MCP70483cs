3fc29cf3cd36885de37b391c894463f484d96090
yadac <doruji1982@gmail.com>
2017-12-17 08:22:40 +0900

    collection array

---
+++ MCP70483cs/Example4/Example4-80.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MCP70483cs.Example4
+{
+    internal class Example4_80
+    {
+        public static void DoProc()
+        {
+            var array = new int[10];
+
+            for (var i = 0; i < array.Length; i++)
+                array[i] = i;
+
+            foreach (var item in array)
+                Console.Write(item);
+
+            string[,] array2D = new string[3, 2]
+            {
+                {
+                    "one", "two"
+                },
+                {
+                    "three", "four"
+                },
+                {
+                    "five", "six"
+                },
+            };
+            Console.WriteLine(array2D[0, 0]);
+            Console.WriteLine(array2D[2, 1]);
+
+            string[][] jaggedArray =
+            {
+                new string[] { "1", "3", "5", "7", "9"},
+                new string[] { "0", "2", "4", "6"},
+                new string[] { "42", "21"},
+            };
+            Console.WriteLine(jaggedArray[0][0]);
+            Console.WriteLine(jaggedArray[2][1]);
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -6,7 +6,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            Example4_79.DoProc();
+            Example4_80.DoProc();
         }
     }
 }

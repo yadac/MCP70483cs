16f2742c062d5c754949b407cd00f995a2a52e00
yadac <doruji1982@gmail.com>
2017-12-17 22:16:17 +0900

    blocking collection

---
+++ MCP70483cs/Exam/ServiceSample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    [ServiceContract]
+    public class ServiceSample
+    {
+        [OperationContract]
+        public void EncryptData(byte[] data)
+        {
+            q.Add(data);
+        }
+
+        private static BlockingCollection<byte[]> q;
+
+        static ServiceSample()
+        {
+            q = new BlockingCollection<byte[]>();
+            new Thread(WorkerMethod).Start();
+            new Thread(WorkerMethod).Start();
+        }
+
+        static void WorkerMethod()
+        {
+            foreach (var data in q.GetConsumingEnumerable())
+            {
+                Encrypt(data);
+            }
+        }
+
+        private static void Encrypt(object data)
+        {
+
+            throw new NotImplementedException();
+        }
+
+
+    }
+}

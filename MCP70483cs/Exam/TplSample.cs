a87ec811b4c75b926555d303b22c127aa4773aaa
yadac <doruji1982@gmail.com>
2017-12-17 20:32:28 +0900

    Task closure

---
+++ MCP70483cs/Exam/TplSample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    internal class TplSample
+    {
+        public IEnumerable<Task> Execute(Action[] jobs)
+        {
+            var tasks = new Task[jobs.Length];
+            for (var i = 0; i < jobs.Length; i++)
+            {
+                // it is possible to different value of i when calling and return.
+                // tasks[i] = Task.Run(() => RunJob(jobs[i], i)); i = index. closure?
+                tasks[i] = new Task(index => RunJob(jobs[(int) index], (int) index), i);
+                tasks[i].Start();
+            }
+            return tasks;
+        }
+
+        public void RunJob(Action job, int index)
+        {
+        }
+    }
+}

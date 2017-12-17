0e0481c3f4d648a8bba521818732c5a041a5a314
yadac <doruji1982@gmail.com>
2017-12-17 21:23:35 +0900

    docbuilder

---
+++ MCP70483cs/Exam/DocBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCP70483cs.Exam
+{
+    internal class DocBuilder
+    {
+        private List<string> dict;
+        private StringBuilder doc;
+
+        private DocBuilder()
+        {
+            InitDictionary();
+        }
+
+        private void InitDictionary()
+        {
+        }
+
+        private StringBuilder BuildDoc(ulong words)
+        {
+            int index = 0;
+            doc = new StringBuilder(5000, 40000);
+            var pick = new Random();
+            for (ulong i = 0; i < words; i++)
+            {
+                // which line should you add here???
+                index = pick.Next(dict.Count());
+                if (doc.Length + dict[index].Length <= doc.MaxCapacity)
+                {
+                    doc.Append(dict[index]);
+                }
+            }
+            return doc;
+        }
+    }
+}

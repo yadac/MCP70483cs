7176709dd7e276a776ff6136fe5dbdd67075d11f
yadac <doruji1982@gmail.com>
2017-12-17 09:40:56 +0900

    custom collection

---
+++ MCP70483cs/Example4/Example4-90.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace MCP70483cs.Example4
+{
+    internal class Example4_90 : List<Person4>
+    {
+        public static void DoProc()
+        {
+            var p1 = new Person4 { Id = 1, Name = "taro", Age = 10 };
+            var p2 = new Person4 { Id = 2, Name = "jiro", Age = 20 };
+            var p3 = new Person4 { Id = 3, Name = "saburo", Age = 30 };
+
+            var list = new Example4_90 { p1, p2, p3 };
+            list.RemoveByAge(20);
+            WriteLine(list.ToString());
+        }
+
+        public void RemoveByAge(int age)
+        {
+            // foreach can not change during loop, but for can change.
+            // "this" is indexer.
+            for (var index = Count - 1; index >= 0; index--)
+                if (this[index].Age == age)
+                    RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var p in this)
+                sb.AppendFormat(p + "\r\n");
+            return sb.ToString();
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -6,7 +6,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            Example4_88.DoProc();
+            Example4_90.DoProc();
         }
     }
 }

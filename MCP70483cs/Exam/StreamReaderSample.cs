42b3321622b2c3678093924e4515e5667bfc5014
yadac <doruji1982@gmail.com>
2017-12-18 20:59:09 +0900

    streamreader

---
+++ MCP70483cs/Exam/StreamReaderSample.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace MCP70483cs.Exam
+{
+    internal class StreamReaderSample
+    {
+        public void DoProc(MemoryStream memoryStream)
+        {
+            using (var reader = new StreamReader(memoryStream))
+            {
+                var data = reader.ReadToEnd();
+            }
+        }
+    }
+}

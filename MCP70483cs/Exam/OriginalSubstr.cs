adb23f910470b1d5de0de8ae6c7c34e14fbe5307
yadac <doruji1982@gmail.com>
2017-12-19 20:19:52 +0900

    string handling

---
+++ MCP70483cs/Exam/OriginalSubstr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MCP70483cs.Exam
+{
+    public class OriginalSubstr
+    {
+        public Tuple<bool, string> DoProc(string source, string search)
+        {
+            var position = source.IndexOf(search, StringComparison.Ordinal);
+            if (position < 0) return null;
+            var remove = source.Remove(position, search.Length);
+            return new Tuple<bool, string>(true, remove);
+        }
+    }
+}

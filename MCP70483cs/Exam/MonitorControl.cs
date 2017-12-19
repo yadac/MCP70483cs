54d34ab0c02cb905d18eda5972848e213dbdef87
yadac <doruji1982@gmail.com>
2017-12-19 19:12:58 +0900

    monitor exclusive

---
+++ MCP70483cs/Exam/MonitorControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    public class MonitorControl<T>
+    {
+        private List<T> list = new List<T>();
+
+        public void Add(T entry)
+        {
+            Monitor.Enter(list);
+            try
+            {
+                list.Add(entry);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                Monitor.Exit(list);
+            }
+        }
+    }
+}

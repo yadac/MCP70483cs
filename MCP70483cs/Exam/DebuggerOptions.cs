8351969e7a5f35a1e85abd3a3edfcff59e969cbc
yadac <doruji1982@gmail.com>
2017-12-20 21:44:50 +0900

    breakpoint condition

---
+++ MCP70483cs/Exam/DebuggerOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    class DebuggerOptions
+    {
+        private static int _count;
+
+        public static void DoProc()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ProcessRequest();
+            }
+        }
+
+        
+        private static void ProcessRequest()
+        {
+            Console.WriteLine(_count++);
+        }
+    }
+}

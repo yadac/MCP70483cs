0c66798d217eefb8d0fe63e3cef1b971c402c18f
yadac <doruji1982@gmail.com>
2017-12-20 23:30:08 +0900

    delegate

---
+++ MCP70483cs/Exam/ErrorEventHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    public delegate void ErrorEventHandler(object sender, ErrorEventArgs e);
+
+    public class ErrorEventArgsSample
+    {
+        private static ErrorEventHandler handler;
+        private static Action<object, ErrorEventArgs> handler2;
+        private static ErrorEventArgs e1 = new ErrorEventArgs("this is a custom error1");
+        private static ErrorEventArgs e2 = new ErrorEventArgs("this is a custom error2");
+
+        public static void DoProc()
+        {
+            handler += (sender, args) =>
+            {
+                Console.WriteLine(args.Error);
+            };
+
+            handler2 += (sender, args) =>
+            {
+                Console.WriteLine(args.Error);
+            };
+
+            handler(null, e1);
+            handler2(null, e2);
+        }
+    }
+
+    public class ErrorEventArgs : EventArgs
+    {
+        public ErrorEventArgs(string error)
+        {
+            Error = error;
+        }
+
+        public string Error { get; }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -9,7 +9,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            QuerySample.DoProc();
+            ErrorEventArgsSample.DoProc();
         }
     }
 }

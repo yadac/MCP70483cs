f495404ffb9051ce6e05895964a74a1480eb590a
yadac <doruji1982@gmail.com>
2017-12-21 20:57:31 +0900

    type parameter

---
+++ MCP70483cs/Exam/IntegerWriter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MCP70483cs.Exam
+{
+    internal class IntegerWriter
+    {
+        public static void DoProc()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+
+            using (var writer = new StreamWriter(@"c:\temp\test\integers.txt"))
+            {
+                list.ForEach(i => writer.WriteLine(i));
+            }
+        }
+    }
+}
---
+++ MCP70483cs/Exam/MainWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    class MainWindow<T> : MyWindow
+        where T : INotifyPropertyChanging, new ()
+    {
+        public T DataContext { get; set; }
+
+        public MainWindow()
+        {
+            this.DataContext = new T();
+        }
+
+        public MainWindow(T viewModel)
+        {
+            this.DataContext = viewModel;
+        }
+    }
+
+    internal class MyWindow
+    {
+    }
+
+    internal class MyContext { }
+}

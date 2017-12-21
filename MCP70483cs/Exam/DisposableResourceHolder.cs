1b0dd8e69b1d7b5aa8c05ccecb82e7358f872c68
yadac <doruji1982@gmail.com>
2017-12-21 20:25:35 +0900

    dispose pattern

---
+++ MCP70483cs/Exam/DisposableResourceHolder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    class DisposableResourceHolder : IDisposable
+    {
+        private SqlConnection resource; // handle to a resource
+
+        public DisposableResourceHolder()
+        {
+            this.resource = null; // actually allocates the unmanaged resrouce. 
+        }
+
+        public void Dispose()
+        {
+            // manually dispose, so suppress gc.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~DisposableResourceHolder()
+        {
+            // if manage resource, relase by gc.
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // if resource is already null, nothing to do.
+                // prevent duplicate relase.
+                if (resource != null) resource.Dispose();
+            }
+        }
+    }
+}

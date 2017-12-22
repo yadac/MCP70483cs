dfbd21040431796c2ea1e0323386de407888791a
yadac <doruji1982@gmail.com>
2017-12-22 21:48:01 +0900

    tcpclient

---
+++ MCP70483cs/Exam/TcpListenerSample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    class TcpListenerSample
+    {
+        public static void DoProc()
+        {
+            var listener = new TcpListener(IPAddress.Any, 500);
+            var client = listener.AcceptTcpClient();
+            var stream = client.GetStream();
+            using (var reader = new StreamReader(stream))
+            {
+                var content =  reader.ReadToEnd();
+            }
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -9,7 +9,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            LabelSample.DoProc();
+            TcpListenerSample.DoProc();
         }
     }
 }

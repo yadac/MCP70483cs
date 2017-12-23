249d28b2584ad6cf70f6ae0cc32e92653ab923d7
yadac <doruji1982@gmail.com>
2017-12-23 12:06:28 +0900

    VariousEmployee

---
+++ MCP70483cs/Exam/VariousEmployeeCreater.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    class VariousEmployeeCreater
+    {
+        public static void DoProc(EmployeeTypeSample code)
+        {
+            switch (code)
+            {
+                case EmployeeTypeSample.Hourly:
+                    Console.WriteLine("houly");
+                    break;
+                case EmployeeTypeSample.Salary:
+                    Console.WriteLine("salaly");
+                    break;
+                case EmployeeTypeSample.Director:
+                    Console.WriteLine("director");
+                    goto case EmployeeTypeSample.Manager;
+                case EmployeeTypeSample.Manager:
+                    Console.WriteLine("manager");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+    }
+
+    enum EmployeeTypeSample
+    {
+        Hourly,
+        Salary,
+        Manager,
+        Director,
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -9,7 +9,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            AsyncReaderSample.DoProc();
+            VariousEmployeeCreater.DoProc();
         }
     }
 }

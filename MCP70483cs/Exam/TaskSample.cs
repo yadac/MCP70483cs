109ef20d42a7f91eb95b1f62350b53fb5e28b360
yadac <doruji1982@gmail.com>
2017-12-24 20:23:49 +0900

    task in tpl

---
+++ MCP70483cs/Exam/TaskSample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    class TaskSample
+    {
+        public static void DoProc()
+        {
+
+        }
+
+        public IEnumerable<Task> Execute(Action[] jobs)
+        {
+            var tasks = new Task[jobs.Length];
+            for (int i = 0; i < jobs.Length; i++)
+            {
+                tasks[i] = new Task((index) => RunJob(jobs[(int)index], (int)index), i);
+            }
+
+            return tasks;
+        }
+
+        internal void RunJob(Action action, int index)
+        {
+
+        }
+    }
+}

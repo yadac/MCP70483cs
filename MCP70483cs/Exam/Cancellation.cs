ae08b0c9e094967cd212f2cd3d77ca4e00346750
yadac <doruji1982@gmail.com>
2017-12-24 22:14:50 +0900

    cancellation

---
+++ MCP70483cs/Exam/Cancellation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    internal class Cancellation
+    {
+        public static IEnumerable<string> GetDataInfoes { get; set; }
+
+        public static void DoProc()
+        {
+            try
+            {
+                var cts = new CancellationTokenSource();
+                var tasks = new List<Task>();
+                foreach (var item in GetDataInfoes)
+                    tasks.Add(Task.Run(() => DoSomething(item, cts))
+                        .ContinueWith(t => Save(t.Result),
+                            TaskContinuationOptions.NotOnRanToCompletion));
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (AggregateException ex)
+            {
+                // flatten = インスタンスを 1 つの新しいインスタンスにフラット化します
+                var canceled = from e in ex.Flatten().InnerExceptions
+                    where e.GetType() == typeof(TaskCanceledException)
+                    select e;
+                foreach (var c in canceled) ProcessCancellation(c);
+            }
+        }
+
+        private static void ProcessCancellation(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task<int> DoSomething(string s, CancellationTokenSource cts)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task Save(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

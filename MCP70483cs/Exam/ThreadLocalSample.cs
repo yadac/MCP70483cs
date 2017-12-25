1779658fb1465de009ec8699c1ec3df4a7dc3818
yadac <doruji1982@gmail.com>
2017-12-26 00:32:02 +0900

    threadlocal

---
+++ MCP70483cs/Exam/ThreadLocalSample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    class ThreadLocalSample
+    {
+        static ThreadLocal<int> threadLocal = new ThreadLocal<int>();
+
+        public static void DoProc()
+        {
+            threadLocal.Value = 123;
+
+            // thread have own value.
+            // 1
+            // -1
+            // 123
+            Task.WhenAll(
+                Task.Run(() => { Console.WriteLine(++threadLocal.Value); }),
+                Task.Run(() => { Console.WriteLine(--threadLocal.Value); })).Wait();
+            Console.WriteLine(threadLocal.Value);
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -9,7 +9,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            DebugAssertion.DoProc();
+            ThreadLocalSample.DoProc();
         }
     }
 }

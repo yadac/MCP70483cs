c93d24b8240120e0a4c8a0efa464092d901fe48f
yadac <doruji1982@gmail.com>
2017-12-25 23:29:03 +0900

    waithandle, manual vs autoresetevent

--- MCP70483cs/Exam/WaitHandleSample.cs
+++ MCP70483cs/Exam/WaitHandleSample.cs
@@ -12,10 +12,11 @@ namespace MCP70483cs.Exam
         static WaitHandle[] handles = new WaitHandle[]
         {
             // このオブジェクトは「シグナル状態」と「非シグナル状態」の2つの状態を持ちます。
-            // 同期イベントにはAutoResetEventとManualResetEventの2種類あります。
-            // signal状態になるまで待機させることができる
-            new AutoResetEvent(false),
-            new AutoResetEvent(false),
+            // (set)signal = thread start / (reset)non-siglal = thread stop
+            // 
+            // false = non-signal
+            new ManualResetEvent(false),
+            new ManualResetEvent(false),
         };
 
 
@@ -26,25 +27,35 @@ namespace MCP70483cs.Exam
 
         public WaitHandleSample()
         {
-            ThreadPool.QueueUserWorkItem(val => CalculateSample((AutoResetEvent)val), handles[0]);
-            ThreadPool.QueueUserWorkItem(val => CalculateSample((AutoResetEvent)val), handles[1]);
+            // non-signal
+            //ThreadPool.QueueUserWorkItem(val => CalculateSample((AutoResetEvent)val), handles[0]);
+            //ThreadPool.QueueUserWorkItem(val => CalculateSample((AutoResetEvent)val), handles[1]);
+            ThreadPool.QueueUserWorkItem(new WaitCallback(CalculateSample), handles[0]);
+            ThreadPool.QueueUserWorkItem(new WaitCallback(CalculateSample), handles[1]);
 
+            // after sleeping, send signal by set()
             Thread.Sleep(3000);
-            (handles[0] as AutoResetEvent).Set();
+            (handles[0] as ManualResetEvent).Set();
             Thread.Sleep(3000);
-            (handles[1] as AutoResetEvent).Set();
+            (handles[1] as ManualResetEvent).Set();
 
             Console.WriteLine("through");
 
+            // AutoEventReset is already turn non-signal. waitall method waits that all of element receive signal, so wait forever...
+            // ManualReset is signaled
             WaitHandle.WaitAll(handles);
-            
+            Summarize();
+        }
+
+        private void Summarize()
+        {
+            Console.WriteLine("finish!");
         }
 
 
         private void CalculateSample(object state)
         {
-            var eve = state as AutoResetEvent;
-            eve.WaitOne();
+            (state as WaitHandle).WaitOne();
             Console.WriteLine("called!!");
         }
     }

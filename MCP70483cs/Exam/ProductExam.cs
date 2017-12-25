e90aa3948088aff81abe4bd90a052b34b42d8612
yadac <doruji1982@gmail.com>
2017-12-25 21:18:08 +0900

    exam summary

---
+++ MCP70483cs/Exam/ProductExam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    internal class ProductExam
+    {
+        // この型またはメンバーには、それが宣言されているアセンブリ内の任意のコードからアクセスできるほか、
+        // 別のアセンブリの派生クラス内からアクセスすることができます。
+        protected internal ProductExam()
+        {
+            
+        }
+    }
+}
---
+++ MCP70483cs/Exam/RootCustomer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace MCP70483cs.Exam
+{
+    public abstract class RootCustomer
+    {
+        public string Name { get; set; }
+
+        public virtual IEnumerable<RootCustomer> GetCustomer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [DataContract]
+    internal class DerivedCustomerSample : RootCustomer
+    {
+        [DataMember] private int age;
+
+        public int Salaly { get; set; }
+
+        public override IEnumerable<RootCustomer> GetCustomer()
+        {
+        }
+    }
+
+    public class GinzaSerializer<T>
+    {
+        private readonly DataContractSerializer selializer;
+
+        public GinzaSerializer()
+        {
+            selializer = new DataContractSerializer(typeof(T));
+        }
+
+        public void Serialize(Stream stream, RootCustomer customer)
+        {
+            selializer.WriteObject(stream, customer);
+        }
+    }
+
+    public class Exam1225
+    {
+        public Exam1225()
+        {
+            var instance = new DerivedCustomerSample();
+            var result = instance.GetCustomer()
+                .Select(c => new {N = c.Name != null ? c.Name : "null-taro"});
+        }
+    }
+}

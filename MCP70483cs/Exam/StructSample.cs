7466eb9a23f92fb838ff39816100812952ccf83e
yadac <doruji1982@gmail.com>
2017-12-26 21:36:43 +0900

    backing fields

---
+++ MCP70483cs/Exam/StructSample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    struct MyStruct
+    {
+        private double _x;
+        private double _y;
+
+        public double X
+        {
+            get { return this._x; }
+            private set { this._x = value; }
+        }
+
+        public MyStruct(double x, double y)
+        {
+            // already assigned X manually
+            // this.X = x;
+            throw new NotImplementedException();
+        }
+    }
+}

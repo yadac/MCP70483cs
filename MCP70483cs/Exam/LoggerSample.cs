0d04bb2104cb6dd7539ed859cb834db66e0deb87
yadac <doruji1982@gmail.com>
2017-12-26 20:27:42 +0900

    stringwriter

---
+++ MCP70483cs/Exam/LoggerSample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    public class LoggerSample
+    {
+        public LoggerSample(TextWriter logDestination)
+        {
+            // the StringWriter uses the Append() when writing message.
+            logDestination.Write("+++++++");
+            logDestination.Write("message");
+            logDestination.Write("+++++++");
+            Console.WriteLine(logDestination.ToString());
+            // logDestination.Flush();
+            logDestination.Dispose();
+        }
+
+        public static void DoProc()
+        {
+            var destination = new StringBuilder();
+
+            // stringWriter = write string to xxx
+            // streamWriter = write stream to xxxStream
+            var writer = new StringWriter(destination);
+            var logger = new LoggerSample(writer);
+            
+        }
+
+        
+    }
+}

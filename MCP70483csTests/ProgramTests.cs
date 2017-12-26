9367edf143990bf02c74e30fa2eb188039ee8feb
yadac <doruji1982@gmail.com>
2017-12-26 14:15:56 +0900

    mstest samle

---
+++ MCP70483csTests/ProgramTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCP70483cs.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        private PrivateType _target;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            Console.WriteLine("TestInit");
+            // _target = new PrivateType(typeof(Program));
+        }
+
+        [TestMethod]
+        public void MainTest()
+        {
+            // Arrange
+
+            // Action
+            Program.Main(new string[] { });
+
+            // Assert
+            Assert.AreEqual(true, true);
+        }
+    }
+}

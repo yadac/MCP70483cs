549b8e67ac65da994bf933e0d6fdbdd28d26d160
yadac <doruji1982@gmail.com>
2017-12-27 21:02:41 +0900

    cast

---
+++ MCP70483cs/Exam/CastSample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP70483cs.Exam
+{
+    class CastSample
+    {
+        public static void DoProc()
+        {
+            
+        }
+
+        public static float DoWork(string a, string b)
+        {
+            // need to Parse() instead (float) casting.
+            return float.Parse(a) + float.Parse(b);
+        }
+    }
+}
--- MCP70483cs/Program.cs
+++ MCP70483cs/Program.cs
@@ -9,7 +9,7 @@ namespace MCP70483cs
     {
         public static void Main(string[] args)
         {
-            CategorySample.DoProc();
+            CastSample.DoProc();
         }
     }
 }
